9ac9ef05ea6dc223efd58f746b7afc29128318fc
Igor F. Canduela <ifcanduela@gmail.com>
2024-03-12 15:47:27 +0100

    UI updates

--- components/MonsterCard.vue
+++ components/MonsterCard.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="bg-white/90 p-3 cursor-pointer hover:bg-orange-100">
+	<div class="bg-white/90 p-3 cursor-pointer hover:bg-emerald-50">
 		<h2 class="mb-2 font-bold">{{ monster.name }}</h2>
 
 		<div class="flex justify-between items-center">
