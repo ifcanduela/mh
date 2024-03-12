35ebc560cfd5e15b0d6f72b74331674a30b3cbda
Igor F. Canduela <ifcanduela@gmail.com>
2024-03-12 15:06:45 +0100

    Restore config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	ssr: false,
-	app: {
-		baseURL: "/mh/",
-	},
 	devtools: { enabled: false },
 	devServer: {
 		port: 3020,
