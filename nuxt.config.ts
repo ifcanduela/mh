5f118daf4e62d314938e784693207a79843af0cf
Igor F. Canduela <ifcanduela@gmail.com>
2024-03-19 15:37:59 +0100

    Just hardcode it

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,7 +33,9 @@ export default defineNuxtConfig({
 	},
 	supabase: {
 		redirect: false,
-		key: process.env.SUPABASE_KEY,
-		url: process.env.SUPABASE_URL,
+		// key: process.env.SUPABASE_KEY,
+		// url: process.env.SUPABASE_URL,
+		url: "https://piumxcungvlrwbwuzomy.supabase.co",
+		key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpdW14Y3VuZ3Zscndid3V6b215Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU5MTk0MzIsImV4cCI6MjAyMTQ5NTQzMn0.GpxlaUSR7GDv_J9Y7S_5VkDKGQHc9gSxm35aCEFhhK8",
 	},
 })
