c381ac672c19a9adcc199e8ca10281adb2e4daa4
Igor F. Canduela <ifcanduela@gmail.com>
2024-03-19 13:32:14 +0100

    adfasf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,12 @@ export default defineNuxtConfig({
 	// ssr: false,
 	devtools: { enabled: false },
 	app: {
-		baseURL: process.env.NUXT_APP_BASE_URL,
+		baseURL: "/mh",
+	},
+	$development: {
+		app: {
+			baseURL: "",
+		},
 	},
 	nitro: {
 		prerender: {
